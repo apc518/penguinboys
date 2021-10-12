5aea91a49be018591a3fd18e97353d72906a9874
Jack Bodine <JackTaylorBodine@gmail.com>
2021-10-12 14:09:44 -0700

    Checks if fields are empty.

--- form.php
+++ form.php
@@ -3,4 +3,18 @@
    echo("Middle name: " . $_POST['m_name'] . "<br />\\n");
    echo("Last name: " . $_POST['l_name'] . "<br />\\n");
    echo("SSN: " . $_POST['ssn'] . "<br />\\n");
+
+
+  if (empty($_POST['f_name'])) {
+    echo "First name is empty!";
+  }
+  if (empty($_POST['m_name'])) {
+    echo "Middle name is empty!";
+  }
+  if (empty($_POST['l_name'])) {
+    echo "Last name is empty!";
+  }
+  if (empty($_POST['ssn'])) {
+    echo "ssn is empty!";
+  }
 ?>
