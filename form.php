fb2d2d88e86ea5de0d409962601b36e144586e5e
Jack Bodine <JackTaylorBodine@gmail.com>
2021-10-12 14:00:04 -0700

    Recreated DB and added form.html and form.php

---
+++ form.php
@@ -0,0 +1,6 @@
+<?php
+   echo("First name: " . $_POST['f_name'] . "<br />\\n");
+   echo("Middle name: " . $_POST['m_name'] . "<br />\\n");
+   echo("Last name: " . $_POST['l_name'] . "<br />\\n");
+   echo("SSN: " . $_POST['ssn'] . "<br />\\n");
+?>
