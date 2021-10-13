29947dfd69c5ca2029f73c8f27bc306b7e54b776
Jack Bodine <JackTaylorBodine@gmail.com>
2021-10-13 16:37:01 -0700

    auto fill fix #3

--- form_body.php
+++ form_body.php
@@ -56,9 +56,9 @@
 
         <label for="ssn">SSN:</label><br>
         <?php
-          if (isset($_GET['ssn'])){
+          if (isset($_GET['passenger_ssn'])){
             echo "<input type='text' id='ssn' name='ssn' placeholder='123-45-6789'
-      pattern='[0-9]{3}-[0-9]{2}-[0-9]{4}' value='" . $_GET['ssn'] ."'>";
+      pattern='[0-9]{3}-[0-9]{2}-[0-9]{4}' value='" . $_GET['passenger_ssn'] ."'>";
           } else {
             echo "<input type='text' id='ssn' name='ssn' placeholder='123-45-6789'
       pattern='[0-9]{3}-[0-9]{2}-[0-9]{4}'>";
